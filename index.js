c7337682203b5f2663b40bb65fbea485e0f55eac
Kristina Kalíková <129938050+luckykristen@users.noreply.github.com>
2024-06-06 21:04:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const items = document.querySelectorAll(".item");
+items.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        removeFocus();
+    item.addEventListener("mouseout", () => {
+        removeFocus();
+    })
+    item.classList.add("selected");
+    })
+})
+
+removeFocus = () => {
+    items.forEach(item => {
+        item.classList.remove('selected');
+    })
+}
